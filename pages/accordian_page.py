96e7545c4b6e28e09db7f57ba0a8f6d00d20a3b6
Naumov Danila <naumovdanila05@gmail.com>
2025-03-27 17:16:56 +0300

    8 домашнее задание

--- pages/accordian_page.py
+++ pages/accordian_page.py
@@ -7,5 +7,8 @@ class Accordian_page(BasePage):
         self.base_url = "https://demoqa.com/accordian"
         super().__init__(driver, self.base_url)
 
-        self.section_content = WebElement(driver, '#section1Content > p')
+        self.section_content_first = WebElement(driver, '#section1Content > p')
+        self.section_content_second_1 = WebElement(driver, '#section2Content > p:nth-child(1)')
+        self.section_content_second_2 = WebElement(driver, '#section2Content > p:nth-child(2)')
+        self.section_content_third = WebElement(driver, '#section3Content > p')
         self.btn_setion_heading = WebElement(driver, '#section1Heading')
